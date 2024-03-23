c47222380ecd42124f1df065be5d2a145b273acc
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2024-03-24 01:23:50 +0300

    Add files via upload

---
+++ Rento/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rento.Data;
+using Rento.Models;
+
+namespace Rento.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RentoContext db = new RentoContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Car_List()
+        {
+            return View(db.Cars.ToList());
+        }
+        public ActionResult Car_Info(int? id)
+        {
+            Car car = db.Cars.Find(id);
+            return View(car);
+        }
+
+
+    }
+}
---
+++ Rento/Data/RentoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Rento.Data
+{
+    public class RentoContext : DbContext
+    {
+
+        public RentoContext() : base("name=RentoContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Rento.Models.Car> Cars { get; set; }
+    }
+}
